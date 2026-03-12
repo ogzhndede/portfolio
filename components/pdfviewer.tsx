0c6e66eeab80cfba5d495688efb32020bacad1e1
ogzhndd <oguzhandede99@gmail.com>
2026-03-12 23:47:10 +0300

    Bug Fix

--- components/pdfviewer.tsx
+++ components/pdfviewer.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { Document, Page, pdfjs } from "react-pdf";
 import { useState } from "react";
+// @ts-ignore
 import "react-pdf/dist/Page/AnnotationLayer.css";
+// @ts-ignore
 import "react-pdf/dist/Page/TextLayer.css";
 
 // Worker ayarını sadece client-side'da yap
 if (typeof window !== 'undefined') {
-  pdfjs.GlobalWorkerOptions.workerSrc = //unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs;
+  pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 }
 
 export default function PdfViewer({ url, scale, pageNumber, onDocumentLoadSuccess }: any) {
